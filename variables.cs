04d0e003288fd161c5efc82b518c73e1125b28ad
Aqdas1234 <bsef21m505@pucit.edu.pk>
2024-07-30 03:08:54 -0700

    variable

---
+++ variables.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace std{
+    class variavbl{
+        static void Main(string[] ags)
+        {
+            //  c# is typed language means we need specify 
+            //the type of a variable at the time of  decleration
+
+            int a ;
+            a=5;
+            Console.WriteLine(a);
+        }
+    }
+}
