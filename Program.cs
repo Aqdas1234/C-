73b7cb005adc6be4ee9f4bfffe120d4e0fcaf8a4
Aqdas1234 <bsef21m505@pucit.edu.pk>
2024-07-30 02:54:22 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+namespace std{
+    public class Program{
+    static void Main(String[] ags)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Aliiiiiii");
+    }
+}
+}
